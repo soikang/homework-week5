d56d6ca36af027e5be0781e3cb028aa7a8408e4d
Soi <skang0806@gmail.com>
2023-10-01 18:35:27 -0600

    moved jquery code to activity.js

--- day1/js/activity.js
+++ day1/js/activity.js
@@ -1,3 +1,10 @@
-$(document).ready(function() {
-
+$(document).ready(function () {
+    // Add a click event handler to all table cells in the tbody
+    $('td').click(function (){
+         // Check if the cell contains the text "Not Available"
+        if ($(this).text().trim() !== "Not Available") {
+        // Toggle the 'highlighted' class on the clicked cell
+        $(this).toggleClass('highlighted');
+        }
+    });
 });
