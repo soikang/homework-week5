d3b21b038d4eccf57106440906e3fe4d6db27b4c
Soi <skang0806@gmail.com>
2023-10-01 19:18:15 -0600

    fixed the heading

--- day1/js/activity.js
+++ day1/js/activity.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // Add a click event handler to all table cells in the tbody
     $('td').click(function (){
          // Check if the cell contains the text "Not Available"
-        if ($(this).text().trim() !== "Not Available") {
+        if (($(this).text().trim() !== "Not Available") && !$(this).is('#unselectable')) {
         // Toggle the 'highlighted' class on the clicked cell
         $(this).toggleClass('highlighted');
         }
